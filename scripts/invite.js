e6fe647e1d0c5916a1debeb819687a64cacd64f7
James Gayfer <james@stembolt.com>
2018-03-12 09:30:09 -0700

    Add redirect if invite link is expired

--- scripts/invite.js
+++ scripts/invite.js
@@ -5,14 +5,7 @@ function page_load() {
   let params = (new URL(document.location)).searchParams
   game_id = params.get("game_id")
   store.set('game_id', game_id)
-  get_game(game_id, set_invite_header)
-}
-
-function set_invite_header(json) {
-  var invite_header = document.getElementById('invite-header')
-  var player_name = json['game']['next_player']
-  console.log(json)
-  invite_header.innerHTML = player_name + ' has invited you to play!'
+  get_game(game_id, retrieve_game_callback)
 }
 
 function accept_invite() {
@@ -24,6 +17,20 @@ function accept_invite() {
 // ******************
 // Internal functions
 // ******************
+
+function set_invite_header(json) {
+  var invite_header = document.getElementById('invite-header')
+  var player_name = json['game']['next_player']
+  invite_header.innerHTML = player_name + ' has invited you to play!'
+}
+
+function retrieve_game_callback(json) {
+  if (json['game_has_started']) {
+    window.location.href = 'link_expired.html'
+  } else {
+    set_invite_header(json)
+  }
+}
 
 function join_game_callback(json) {
   store.set('client_id', json['client_id'])
